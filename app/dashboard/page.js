ee845366545f26e729947f53b72b203cd2272217
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2024-07-30 01:55:14 -0400

    added V0 config

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Dashboard } from "@/components/dashboard"
+
+const DashboardMain = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default DashboardMain
